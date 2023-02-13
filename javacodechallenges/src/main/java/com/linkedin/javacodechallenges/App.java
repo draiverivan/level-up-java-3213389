93952677622b0918ce6c3cbc042551fe3d1e6571
draiverivan <124898717+draiverivan@users.noreply.github.com>
2023-02-13 20:18:41 +0000

    Implement Person class

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,9 +1,13 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
+public class App {
+    public static void main(String[] args) {
+        Person p1 = new Person("Ron", "Freeman", 37);
+        Person p2 = new Person("Taylor", "Warren", 29);
+        Person p3 = new Person("Sarah", "Ford", 52);
+        p1.setAge(12);
+        p1.introduceYourself();
+        p2.introduceYourself();
+        p3.introduceYourself();
     }
 }
