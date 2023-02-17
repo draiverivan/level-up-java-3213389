83a2a4292198051135b9705f3bf22b5745e8746b
draiverivan <124898717+draiverivan@users.noreply.github.com>
2023-02-17 21:41:13 +0000

    Implement 100 days from now in two ways

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,32 @@
 package com.linkedin.javacodechallenges;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+
 public class App 
 {
     // Create function to calculate the date that's 
     // 100 days from now
+    public static Date calculateHundredDaysFromNow() {
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.DATE, 100);
+        return c.getTime();
+    }
+    
+    public static LocalDate calculateHundredDaysFromNow(LocalDate today) {
+        Period hundredDays = Period.ofDays(100);
+        return today.plus(hundredDays);
+    }
 
     public static void main( String[] args )
     {
-        System.out.println("100 days from now is... " 
+        LocalDate today = LocalDate.now(ZoneId.of("America/New_York"));
+        System.out.println("100 days from now is... " +
+                calculateHundredDaysFromNow(today));
+        System.out.println("100 days from now is... "+  calculateHundredDaysFromNow()
             /* add function call */);
     }
 }
